6f20fbc16c4a85feefb21d79e1cd9eb77a6ae76a
elvisonob <elvisonob@yahoo.com>
2026-07-14 06:13:02 +0100

    editTodo added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,13 +13,32 @@ type TodoListProps = {
 
 function TodoList({ todos, removeTodo, onEditTodo }: TodoListProps) {
   const [editingId, setEditingId] = useState<number | null>(null);
-  const [editText, setEditText] = useState('');
+  const [editText, setEditText] = useState<string>('');
+
+  function startEditing(id: number, text: string) {
+    setEditingId(id);
+    setEditText(text);
+  }
+
+  function saveEdit(id: number, text: string) {
+    onEditTodo(id, text);
+    setEditingId(null);
+    setEditText('');
+  }
+
+  // if an edit button is clicked, the input dialog box should open up
+  // and then we have the save button showing
+  // after editing is done, the save button is clicked
+  // new update takes shape
 
   return (
     <div>
       <h2>LIST OF TODOS</h2>
       <ul>
         {todos.map((todo) => (
+        {editingId === todo.id? <input value={editText} onChange={(e)=> setEditText(e.target.value)}/>}
+
+
           <li>{todo.text}</li>
         ))}
       </ul>
