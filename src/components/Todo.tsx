1a936cafdf257723838dd41bf7d28cff3db2eef0
elvisonob <elvisonob@yahoo.com>
2026-07-28 05:24:03 +0100

    cementing from scratch

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,40 @@
 import { useState } from 'react';
 import TodoList from './TodoList';
 
-//export default Todo;
+type Todo = {
+  id: number;
+  text: string;
+};
+
+function Todo() {
+  const [todoText, setTodoText] = useState('');
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  function onChangeTodo(e: React.ChangeEvent<HTMLInputElement>) {
+    setTodoText(e.target.value);
+  }
+
+  function onSubmitTodo() {
+    setTodoList((prev) => {
+      return [...prev, { id: Math.random(), text: todoText }];
+    });
+
+    console.log(todoText);
+    setTodoText('');
+  }
+
+  return (
+    <div>
+      <h1>TODO APP</h1>
+      <h2>Add Todo</h2>
+      <input id="text" type="text" value={todoText} onChange={onChangeTodo} />
+      <button onClick={onSubmitTodo}>Submit</button>
+      <div>
+        <h3>LIST OF TODO</h3>
+        <TodoList />
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,1 +1,13 @@
 import { useState } from 'react';
+
+function TodoList() {
+  return (
+    <div>
+      <ul>
+        <li>One Love</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
